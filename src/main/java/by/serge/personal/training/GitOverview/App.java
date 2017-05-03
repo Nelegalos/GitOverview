f0408b5382bce2359c00117bfce49490b5dd352d
Serge Z <r_u@inbox.ru>
2017-05-03 11:33:13 +0300

    Initial commit of the progect

---
+++ src/main/java/by/serge/personal/training/GitOverview/App.java
@@ -0,0 +1,13 @@
+package by.serge.personal.training.GitOverview;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
